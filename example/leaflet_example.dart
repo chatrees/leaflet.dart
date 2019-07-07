2969a27e15c9c11c8b5c450c7aeb0f9e9c162651
Chatree Srichart <chatree.srichart@gmail.com>
2019-07-07 13:07:44 +0700

    Initialize repository

---
+++ example/leaflet_example.dart
@@ -0,0 +1,6 @@
+import 'package:leaflet/leaflet.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/leaflet.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library leaflet;
+
+export 'src/leaflet_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
