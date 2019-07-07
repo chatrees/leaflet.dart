8055db28409bb3e7ca6f87560ea9b2c66e59a4a5
Chatree Srichart <chatree.srichart@gmail.com>
2019-07-08 00:09:36 +0700

    Initialize TileLayer class

--- example/leaflet_example.dart
+++ example/leaflet_example.dart
@@ -1,6 +1,5 @@
 import 'package:leaflet/leaflet.dart';
 
 main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+
 }
--- lib/src/leaflet_base.dart
+++ lib/src/leaflet_base.dart
@@ -1,6 +1,56 @@
 // TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'dart:js';
+
+abstract class JsObjectWrapper {
+
+  JsObject _jsObject;
+
+  JsObjectWrapper(this._jsObject);
+}
+
+///
+/// Layer
+/// @see https://leafletjs.com/reference-1.5.0.html#layer
+///
+class LayerOptions {
+  String pane;
+  String attribution;
+}
+
+///
+/// TileLayer
+/// @see https://leafletjs.com/reference-1.5.0.html#tilelayer
+///
+class TileLayer extends JsObjectWrapper {
+  TileLayer(JsObject jsObject) : super(jsObject);
+}
+
+class TileLayerOptions extends LayerOptions {
+  int minZoom;
+}
+
+class LatLng extends JsObjectWrapper {
+  LatLng(JsObject jsObject) : super(jsObject);
+}
+
+class Map extends JsObjectWrapper {
+  Map(JsObject jsObject) : super(jsObject);
+
+  void setView(LatLng latLng, int zoom) {
+    _jsObject.callMethod('setView', [latLng._jsObject, zoom]);
+  }
+}
+
+Map map(String id) {
+  JsObject jsObject = context['L'].callMethod('map', [id]);
+  return Map(jsObject);
+}
+
+LatLng latLng(double latitude, double longitude) {
+  return LatLng(context['L'].callMethod('latLng', [latitude, longitude]));
+}
+
+TileLayer tileLayer(String urlTemplate, {TileLayerOptions options}) {
+  return TileLayer(context['L'].callMethod('tileLayer', [urlTemplate]));
 }
